91ac1c1890db8925a6b87e62eefe1809cf9725ad
cmc228 <cmc228@bath.ac.uk>
2025-02-04 17:32:47 +0000

    test

--- test/test_engineering.py
+++ test/test_engineering.py
@@ -8,7 +8,7 @@ PROCESSED_PATH = os.path.abspath(os.path.join(BASE_DIR, "../data/processed/"))
 def test_features_file():
     """Test for features.csv """
     file_path = os.path.join(PROCESSED_PATH, "features.csv")
-    assert os.path.exists(file_path), f"{file_path} does not exist."
+    assert os.path.exists(file_path), f"{file_path} does not exist "
 
     df = pd.read_csv(file_path)
 
@@ -17,6 +17,6 @@ def test_features_file():
                         "weekly_active_users_index", "spend_per_user"]
 
     for column in required_columns:
-        assert column in df.columns, f"Column '{column}' is missing in features.csv."
+        assert column in df.columns, f"Column '{column}' is missing in features.csv "
 
-    assert not df.isnull().values.any(), "features.csv contains null values."
+    assert not df.isnull().values.any(), "features.csv contains null values "
